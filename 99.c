b54bb0f00bc2b786f1e81c706cc181fef64c2dcc
Fumiyuki Shimizu <fumiyuki.shimizu@emtech-academy.com>
2024-05-03 22:51:40 +0900

    reduced cost of calc.

--- 99.c
+++ 99.c
@@ -5,8 +5,8 @@ int main(void)
   int kk;
   for (kk = 0; kk <= 9 * 8 + 8; ++kk)
   {
-    printf("%2d%c", (kk / 9 + 1) * (kk % 9 + 1),
-           (1 - kk % 9 / 8) * ' ' + kk % 9 / 8 * '\n');
+    printf("%2d%c", (1 + kk / 9) * (1 + kk % 9),
+           ' ' + kk % 9 / 8 * ('\n' - ' '));
   }
   return 0;
 }
