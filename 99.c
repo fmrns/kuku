9cea14c47d57e5cd0babe6ffa682e102b2ba7969
Fumiyuki Shimizu <fumiyuki.shimizu@emtech-academy.com>
2024-05-05 11:03:56 +0900

    defined fmtd.

--- 99.c
+++ 99.c
@@ -2,8 +2,10 @@
 
 #ifdef IN_JAPAN
 #define MAX (9)
+#define FMTD "%2d"
 #else
 #define MAX (12)
+#define FMTD "%3d"
 #endif
 
 int main(void)
@@ -11,7 +13,7 @@ int main(void)
   int kk;
   for (kk = 0; kk < MAX * MAX; ++kk)
   {
-    printf("%3d%c", (1 + kk / MAX) * (1 + kk % MAX),
+    printf(FMTD "%c", (1 + kk / MAX) * (1 + kk % MAX),
            ' ' + kk % MAX / (MAX - 1) * ('\n' - ' '));
   }
   return 0;
